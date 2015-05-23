81a3cf45a28c73d03eeb6be03634a474b126c3c3
Cyrille <cyrillef@free.fr>
2015-05-23 13:35:17 +0200

    using std naming convention

--- routes/api.js
+++ routes/api.js
@@ -15,38 +15,25 @@
 // DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////////////////
-var fs = require('fs');
-var credentials ;
-if ( !fs.existsSync ('credentials.js') ) {
-	console.log ('No credentials.js file present, assuming using CONSUMERKEY & CONSUMERSECRET system variables.') ;
-    credentials =require('../credentials_') ;
-} else {
-	credentials =require('../credentials') ;
-}
-var express = require('express');
-var request = require('request');
+var credentials =(require ('fs').existsSync ('credentials.js') ?
+	  require('../credentials')
+	: (console.log ('No credentials.js file present, assuming using CONSUMERKEY & CONSUMERSECRET system variables.'), require('../credentials_'))) ;
+var express =require ('express') ;
+var request =require ('request') ;
 
-var router = express.Router();
+var router =express.Router () ;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Generates access token
 ///////////////////////////////////////////////////////////////////////////////
-router.get('/token', function (req, res) {
-    var params = {
-        client_id: credentials.ConsumerKey,
-        client_secret: credentials.ConsumerSecret,
-        grant_type: 'client_credentials'
-    } ;
-
-    request.post(
-        credentials.BaseUrl + '/authentication/v1/authenticate',
-        { form: params },
-
+router.get ('/token', function (req, res) {
+    request.post (
+        credentials.Authentication,
+        { form: credentials.credentials },
         function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                res.send(body);
-            }
-        });
-});
+            if ( !error && response.statusCode == 200 )
+                res.send (body) ;
+        }) ;
+}) ;
 
-module.exports = router;
+module.exports =router ;
